f4978d6633bb3f4ca727871c188cf5dd989bbf20
ชื่wutthinan2015 <wutthinan2015.k@gmail.com>
2026-01-11 16:42:00 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,11 @@
+<template lang="">
+    <div>
+        <h1>contact to Wutthinan Kuthaisong </h1>
+        <h3>Tel 0981741352</h3>
+        <img alt="Vue logo" src="../assets/01.png">
+
+    </div>
+</template>
+
+
+
